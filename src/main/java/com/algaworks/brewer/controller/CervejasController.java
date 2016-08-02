ae8ee3e97bd28e71b3f6988faa903b992b147a5e
Anderson Home <agsalle@gmail.com>
2016-08-01 22:06:59 -0300

    Aula 9.6

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,43 +1,67 @@
 package com.algaworks.brewer.controller;
 
+import java.util.Optional;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
+	@Autowired
+	private Cervejas cervejas;
+	
+	@Autowired
+	private Estilos estilos;
+	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
+	public ModelAndView novo(Cerveja cerveja) {
 		
-//		if (logger.isDebugEnabled()){
-			logger.info("O objeto cerveja eh: ");
-//		}
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 		
-		return "cerveja/CadastroCerveja";
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return novo(cerveja);
-		}
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+//		if (result.hasErrors()) {
+//			return novo(cerveja);
+//		}
 		
 		// Salvar no banco de dados...
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>> sku: " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		System.out.println(">>> sabor:" + cerveja.getSabor());
+		System.out.println(">>> Origem:" + cerveja.getOrigem());
+		System.out.println(">>> Nome:" + cerveja.getNome());
+		System.out.println(">>> Descricao:" + cerveja.getDescricao());		
+		System.out.println(">>> Teor alcoolico:" + cerveja.getTeorAlcoolico());
+		System.out.println(">>> Valor:" + cerveja.getValor());
+		System.out.println(">>> Comissao:" + cerveja.getComissao());
+		System.out.println(">>> Estoque:" + cerveja.getQuantidadeEstoque());
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 	
